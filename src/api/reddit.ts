ccccfea8810fd06f8210f29b23e5a010ae9ac523
devforgely <ljyu23@gmail.com>
2026-03-30 21:14:20 +0100

    chore: add proxy to make it work on github

--- src/api/reddit.ts
+++ src/api/reddit.ts
@@ -1,21 +1,22 @@
 export const API_ROOT = 'https://www.reddit.com';
+const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(API_ROOT)}`;
 
 export const getSubredditPosts = async (subreddit: string) : Promise<RedditPost[]> => {
-  const response = await fetch(`${API_ROOT}${subreddit}.json`);
+  const response = await fetch(`${proxyUrl}${subreddit}.json`);
   const json: RedditResponse<RedditPost> = await response.json();
 
   return json.data.children.map((post) => post.data);
 };
 
 export const getSubreddits = async () : Promise<RedditSubreddit[]> => {
-  const response = await fetch(`${API_ROOT}/subreddits.json`);
+  const response = await fetch(`${proxyUrl}/subreddits.json`);
   const json: RedditResponse<RedditSubreddit> = await response.json();
 
   return json.data.children.map((subreddit) => subreddit.data);
 };
 
 export const getPostComments = async (permalink: string) : Promise<RedditComment[]> => {
-  const response = await fetch(`${API_ROOT}${permalink}.json`);
+  const response = await fetch(`${proxyUrl}${permalink}.json`);
   const json: [RedditResponse<RedditPost>, RedditResponse<RedditComment>] = await response.json();
 
   return json[1].data.children.map((subredditComment) => subredditComment.data);
